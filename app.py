629c18885998f2c07062e1d7c5450106bc2934dc
pongpatcharape <pongpatchara.pe@gmail.com>
2026-08-27 16:01:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.set_page_config(
     page_title="NOTAM AREA GENERATOR",
     page_icon="✈️",
-    page_layout="wide",
+    layout="wide",
     initial_sidebar_state="expanded"
 )
 
